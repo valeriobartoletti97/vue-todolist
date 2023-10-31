363a12fa84022a50f52786f5609cbe5df6243add
valeriobartoletti97 <valerio.bartoletti97gmail.com>
2023-10-31 16:57:45 +0100

    added tasks array

--- js/script.js
+++ js/script.js
@@ -3,7 +3,39 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-           ciao: 'ciao'
+           tasks: [
+            {
+                id: 1,
+                text: 'Shoulder Press',
+                done:false
+            },
+            {
+                id:2,
+                text: 'Squat',
+                done:false
+            },
+            {
+                id:3,
+                text: 'Arnold Press',
+                done:false
+            },
+            {
+                id:4,
+                text: 'Deltoide Posteriore',
+                done:false
+            },
+            {
+                id:5,
+                text: 'Frog Jump',
+                done:false
+            },
+            {
+                id:6,
+                text: 'Pettorali con manubri',
+                done:false
+            },
+           ],
+           lastId: 6
         }
     },
     methods:{
